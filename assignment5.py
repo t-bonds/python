057dbcbdb2ddfdd6f94d87d3ff9c1f3c4d60d2ec
Tanner Bonds <tjbonds98@gmail.com>
2019-09-23 22:46:01 -0500

    Update

--- assignment5.py
+++ assignment5.py
@@ -1,47 +1,46 @@
-#Sam Lyons
-#CIS-294
-#2/23/19
-#Assignment 5
+# Sam Lyons
+# CIS-294
+# 2/23/19
+# Assignment 5
 
 """A program that returns values pertaining to a grocery store's stocks and prices
 in order to calculate a bill."""
 
 shopping_list = ["banana", "orange", "banana", "pear", "orange", "banana"]
 
-stock = {  #dictionary containing the stock of the grocery items.
-	
-	"banana": 6,
-	"apple": 0,
-	"orange": 32,
-	"pear": 15
+stock = {  # dictionary containing the stock of the grocery items.
+
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
 }
 
-prices = { #dictionary containing the prices of the grocery items.
-	
-	"banana": 4,
-	"apple": 2,
-	"orange": 1.5,
-	"pear": 3
+prices = {  # dictionary containing the prices of the grocery items.
+
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
 }
 
 
-def compute_bill(food): #function that computes the bills of the total items in the shopping list.
+# function that computes the bills of the total items in the shopping list.
+def compute_bill(food):
 
-	total = 0
-	for item in food:
-		if stock[item] > 0:
-			total += prices[item]
-			stock[item] -= 1
-	return total
-
-
-
+    total = 0
+    for item in food:
+        if stock[item] > 0:
+            total += prices[item]
+            stock[item] -= 1
+    return total
 
 
-def main(): #main function with outputs.
+def main():  # main function with outputs.
 
-	total = str(compute_bill(shopping_list))
+    total = str(compute_bill(shopping_list))
 
-	print("%s %s" % ("Grocery List: $", total))
+    print("%s %s" % ("Grocery List: $", total))
 
-main()	
+
+main()
